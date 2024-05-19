f52d97ec9398944ba171bdc6b2c64f90b1ba9645
Bruno Cesar de Lima de Paula <mbrunocesar@gmail.com>
2024-05-19 00:15:42 -0300

    updates to make use of new java jvm version

--- setup-builder/prerequisites/install-maven.sh
+++ setup-builder/prerequisites/install-maven.sh
@@ -5,17 +5,17 @@ sudo rm /etc/profile.d/maven.sh
 export JAVA_PATH=$(which java)
 
 sudo cat >/etc/profile.d/maven.sh <<'EOL'
-export JAVA_HOME=${JAVA_PATH}
+export JAVA_HOME=/usr/lib/jvm/java-1.21.0-openjdk-amd64
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
-export PATH=${M2_HOME}/bin:${PATH}
+export PATH=$JAVA_HOME/bin:/opt/maven/apache-maven-3.8.8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:.:/snap/bin
 EOL
 
 sudo chmod +x /etc/profile.d/maven.sh
 
-export JAVA_HOME=${JAVA_PATH}
+export JAVA_HOME=/usr/lib/jvm/java-1.21.0-openjdk-amd64
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
-export PATH=${M2_HOME}/bin:${PATH}
+export PATH=$JAVA_HOME/bin:/opt/maven/apache-maven-3.8.8/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:.:/snap/bin
 
 source /etc/profile.d/maven.sh
